02c124fc54d23dafa8f0608fed46c8c73b25aa52
Aram Simonian <aram.contacts@gmail.com>
2019-10-19 16:48:13 +0100

    Added trim_field + test & test_business_field_import_2

--- data_field_cleanup.py
+++ data_field_cleanup.py
@@ -1,2 +1,11 @@
 def titlize_field(value):
     return value.title()
+
+def trim_field(value):
+    # Remove leading/trailing whitespace
+    output = value.strip()
+    if output[-1] == ".":
+        # Remove any trailing full stop
+        output = output[:-1]
+
+    return output
--- tests/test_data_field_cleanup.py
+++ tests/test_data_field_cleanup.py
@@ -1,7 +1,19 @@
 import data_field_cleanup as dfc
 
-def test_business_field_import():
+def test_business_field_import_1():
     input = "MT ALBERT PHARMACY"
     expected = "Mt Albert Pharmacy"
     actual = dfc.titlize_field(input)
+    assert actual == expected
+
+def test_business_field_import_2():
+    input = "p.p.p pet product providers"
+    expected = "P.P.P Pet Product Providers"
+    actual = dfc.titlize_field(input)
+    assert actual == expected
+
+def test_title_field_import():
+    input = "Mr."
+    expected = "Mr"
+    actual = dfc.trim_field(input)
     assert actual == expected
