6adeb4a1d30fbc830b85eb943be9e28dc3874ec2
Aram Simonian <aram.contacts@gmail.com>
2019-10-20 00:54:45 +0100

    Added date conversion code & test

--- data_field_cleanup.py
+++ data_field_cleanup.py
@@ -1,3 +1,6 @@
+import datetime
+import re
+
 def titlize_field(value):
     return value.title()
 
@@ -8,4 +11,17 @@ def trim_field(value):
         # Remove any trailing full stop
         output = output[:-1]
 
+    return output
+
+def format_date_field(value):
+    pat_mdy = r"^\d{1,2}\/\d{1,2}\/\d{2}$"
+    pat_mdY = r"^\d{1,2}\/\d{1,2}\/\d{4}$"
+    value_format = ""
+
+    if re.match(pat_mdy, value):
+        value_format = '%m/%d/%y'
+    elif re.match(pat_mdY, value):
+        value_format = '%m/%d/%Y'
+
+    output = datetime.datetime.strptime(value, value_format).strftime('%d/%m/%Y')
     return output
--- tests/test_data_field_cleanup.py
+++ tests/test_data_field_cleanup.py
@@ -1,19 +1,25 @@
 import data_field_cleanup as dfc
 
 def test_business_field_import_1():
-    input = "MT ALBERT PHARMACY"
-    expected = "Mt Albert Pharmacy"
+    input = 'MT ALBERT PHARMACY'
+    expected = 'Mt Albert Pharmacy'
     actual = dfc.titlize_field(input)
     assert actual == expected
 
 def test_business_field_import_2():
-    input = "p.p.p pet product providers"
-    expected = "P.P.P Pet Product Providers"
+    input = 'p.p.p pet product providers'
+    expected = 'P.P.P Pet Product Providers'
     actual = dfc.titlize_field(input)
     assert actual == expected
 
 def test_title_field_import():
-    input = "Mr."
-    expected = "Mr"
+    input = 'Mr.'
+    expected = 'Mr'
     actual = dfc.trim_field(input)
+    assert actual == expected
+
+def test_dob_field_import():
+    input = '3/21/1951'
+    expected = '21/03/1951'
+    actual = dfc.format_date_field(input)
     assert actual == expected
