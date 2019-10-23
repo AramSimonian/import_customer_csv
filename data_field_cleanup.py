8423aa7797a51eb2722728228d5ebb965913ee1c
Aram Simonian <aram.contacts@gmail.com>
2019-10-23 19:11:28 +0100

    format_date_field handles future year; added new date tests, renamed old ones

--- data_field_cleanup.py
+++ data_field_cleanup.py
@@ -1,5 +1,6 @@
 import datetime
 import re
+import locale
 
 def titlize_field(value):
     return value.title()
@@ -7,13 +8,14 @@ def titlize_field(value):
 def trim_field(value):
     # Remove leading/trailing whitespace
     output = value.strip()
+    # Remove any trailing full stop
     if output[-1] == ".":
-        # Remove any trailing full stop
         output = output[:-1]
 
     return output
 
 def format_date_field(value):
+    locale.setlocale(locale.LC_ALL, 'en_GB')
     pat_mdy = r"^\d{1,2}\/\d{1,2}\/\d{2}$"
     pat_mdY = r"^\d{1,2}\/\d{1,2}\/\d{4}$"
     value_format = ""
@@ -23,5 +25,11 @@ def format_date_field(value):
     elif re.match(pat_mdY, value):
         value_format = '%m/%d/%Y'
 
-    output = datetime.datetime.strptime(value, value_format).strftime('%d/%m/%Y')
+    # Prepare the date value for output
+    output = datetime.datetime.strptime(value, value_format)
+    # Basic check to correct the year
+    if output.year > datetime.datetime.now().year:
+        output = output.replace(year=output.year-100)
+    output = output.strftime('%d/%m/%Y')
+
     return output
--- tests/test_data_field_cleanup.py
+++ tests/test_data_field_cleanup.py
@@ -1,25 +1,63 @@
+import pytest
 import data_field_cleanup as dfc
 
-def test_business_field_import_1():
+def test_business_field_import_all_capitals():
     input = 'MT ALBERT PHARMACY'
     expected = 'Mt Albert Pharmacy'
     actual = dfc.titlize_field(input)
     assert actual == expected
 
-def test_business_field_import_2():
+def test_business_field_import_with_full_stops():
     input = 'p.p.p pet product providers'
     expected = 'P.P.P Pet Product Providers'
     actual = dfc.titlize_field(input)
     assert actual == expected
 
-def test_title_field_import():
+def test_title_field_import_trim_full_stop():
     input = 'Mr.'
     expected = 'Mr'
     actual = dfc.trim_field(input)
     assert actual == expected
 
-def test_dob_field_import():
+def test_dob_field_import_mmddyyyy():
+    input = '03/21/1951'
+    expected = '21/03/1951'
+    actual = dfc.format_date_field(input)
+    assert actual == expected
+
+def test_dob_field_import_mddyyyy():
     input = '3/21/1951'
     expected = '21/03/1951'
     actual = dfc.format_date_field(input)
     assert actual == expected
+
+def test_dob_field_import_mmdyyyy():
+    input = '03/1/1951'
+    expected = '01/03/1951'
+    actual = dfc.format_date_field(input)
+    assert actual == expected
+
+def test_dob_field_import_mdyyyy():
+    input = '3/1/1951'
+    expected = '01/03/1951'
+    actual = dfc.format_date_field(input)
+    assert actual == expected
+
+def test_dob_field_import_mdyy():
+    input = '3/1/51'
+    expected = '01/03/1951'
+    actual = dfc.format_date_field(input)
+    assert actual == expected
+
+def test_dob_field_import_invalid_month():
+    input = '123/1/51'
+    
+    with pytest.raises(Exception):
+        dfc.format_date_field(input)
+    
+def test_dob_field_import_invalid_day():
+    input = '12/44/51'
+    
+    with pytest.raises(Exception):
+        dfc.format_date_field(input)
+    
