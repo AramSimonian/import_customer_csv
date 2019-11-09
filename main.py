c9cea215a512f8263961c4b6135d389497e239ea
Aram Simonian <aram.contacts@gmail.com>
2019-11-09 23:03:20 +0000

    Minor reformatting, some commenting

--- main.py
+++ main.py
@@ -4,17 +4,28 @@ import numpy as np
 def add_contact():
     pass
 
-def get_data(filename):
+def get_data_row(filename):
+    # open the file
     with open(filename, "r") as cust:
         data_reader = csv.reader(cust)
         for row in data_reader:
+            # return a row from the file
             yield row
 
-csv_data = get_data('contact_list .csv')
-header_row = next(csv_data)
-print("header:")
-print(header_row)
+def upload_csv(filename):
+    # get a handle on the generator used to upload the file
+    csv_data = get_data_row(filename)
 
-for data_row in csv_data:
-    print(data_row)
+    # the first row is assumed to be the header row
+    # and is to be discarded
+    # TODO: need checks on how well-formed the file is?
+    next(csv_data)
+    # print("header:")
+    # print(header_row)
 
+    # now work through each row
+    for data_row in csv_data:
+        print(data_row)
+
+
+upload_csv('contact_list .csv')
