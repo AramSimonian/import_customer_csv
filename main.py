241c32081a524fbfec0f8348aa18e3750263fc52
Aram Simonian <aram.contacts@gmail.com>
2019-11-09 11:50:59 +0000

    Slight refactoring to get the csv file generator working

--- main.py
+++ main.py
@@ -1,11 +1,20 @@
 import csv
 import numpy as np
 
+def add_contact():
+    pass
+
 def get_data(filename):
     with open(filename, "r") as cust:
         data_reader = csv.reader(cust)
         for row in data_reader:
             yield row
 
-for row in get_data('contact_list .csv'):
-    print(row)
+csv_data = get_data('contact_list .csv')
+header_row = next(csv_data)
+print("header:")
+print(header_row)
+
+for data_row in csv_data:
+    print(data_row)
+
